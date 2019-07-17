2e60947163c16d874346f19efaab2cd54561946b
xuehao.weng <42528849+Pywww@users.noreply.github.com>
2019-07-17 14:40:00 +0800

    'baidu/mysql-error'

---
+++ apps/books/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+# Create your models here.
+class Publisher(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=60)
+    state_province = models.CharField(max_length=30)
+    country = models.CharField(max_length=50)
+    website = models.URLField()
+
+    class Meta:
+        # 自定义表名称
+        db_table = 'Publisher_table'
+        verbose_name = 'publisher'
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    email = models.EmailField()
+
+    class Meta:
+        db_table = 'Author_table'
+        verbose_name = 'author'
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    authors = models.ManyToManyField(Author)
+    publisher = models.ForeignKey(Publisher,on_delete=models.CASCADE)
+    publication_date = models.DateField()
+
+    class Meta:
+        db_table = 'Book_table'
+        verbose_name = 'book'
