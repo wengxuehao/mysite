9d067125864a58be2c0d015779894aefe046054e
xuehao.weng <42528849+Pywww@users.noreply.github.com>
2019-07-18 10:41:32 +0800

    '修改/admin/name'

--- apps/books/admin.py
+++ apps/books/admin.py
@@ -4,27 +4,26 @@ from django.contrib import admin
 from apps.books.models import Publisher, Author, Book
 
 
-class Publisher_Admin(admin.ModelAdmin):
+class PublisherAdmin(admin.ModelAdmin):
     list_filter = ['name']  # 右侧筛选
     list_display = ['name', 'address']
 
 
-admin.site.register(Publisher, Publisher_Admin)
+admin.site.register(Publisher, PublisherAdmin)
 
 
-class Author_Admin(admin.ModelAdmin):
+class AuthorAdmin(admin.ModelAdmin):
     list_filter = ['first_name']  # 右侧筛选--过滤器
     search_fields = ['first_name']  # 在列表的顶部增加一个搜索框
     list_display = ['first_name', 'last_name', 'email']
 
 
+admin.site.register(Author, AuthorAdmin)
 
-admin.site.register(Author, Author_Admin)
 
-
-class Book_Admin(admin.ModelAdmin):
+class BookAdmin(admin.ModelAdmin):
     list_display = ['title', "publisher", 'publication_date']
     fields = ('title', 'authors', 'publisher', 'publication_date')
 
 
-admin.site.register(Book, Book_Admin)
+admin.site.register(Book, BookAdmin)
